a976dd592e27a475a63e58a8ce39333de6eb79cf
Vamshi Krishna Pothuka <pvamshi@gmail.com>
2021-06-12 01:29:46 +0200

    add firebase connection

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,40 @@ import * as React from "react";
 import { BaseEditor, createEditor, Descendant } from "slate";
 import { Editable, ReactEditor, Slate, withReact } from "slate-react";
 import logTree from "console-log-tree";
+import { debounce } from "debounce";
 
 import withLists from "./with-lists";
 import { ElementNode } from "./models";
 import handleKeys from "./hotkeys";
+import firebase from "firebase";
+import { useEffect } from "react";
+import SkeletonButton from "antd/lib/skeleton/Button";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDe0MkMRDzySCZA9Bx_0OWNeZcfkBkzB6M",
+  authDomain: "qtags-app.firebaseapp.com",
+  databaseURL:
+    "https://qtags-app-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "qtags-app",
+  storageBucket: "qtags-app.appspot.com",
+  messagingSenderId: "876480915268",
+  appId: "1:876480915268:web:f37cb36be791cd15729911",
+  measurementId: "G-TZYHCTL9GG",
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+function writeUserData(id: string, data: any) {
+  console.log("writing");
+  firebase
+    .database()
+    .ref("nodes/" + id)
+    .set(data)
+    .then(() => {
+      console.log("done writinf");
+    });
+}
+const update = debounce(writeUserData, 1000);
 declare module "slate" {
   interface CustomTypes {
     Editor: BaseEditor & ReactEditor;
@@ -31,14 +60,36 @@ const App = () => {
   const [value, setValue] = useState<Descendant[]>([
     { type: "paragraph", id: nanoid(), children: [{ text: "starting" }] },
   ]);
+  useEffect(() => {
+    const dbRef = firebase.database().ref();
+    dbRef
+      .child("nodes")
+      .child("test")
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          setValue(snapshot.val());
+          console.log(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [setValue]);
   logTree.log(log(value));
   console.log(value);
+  const updateData = (data: Descendant[]) => {
+    setValue(data);
+    update("test", data);
+  };
   return (
     // Add the editable component inside the context.
     <Slate
       editor={editor}
       value={value}
-      onChange={(newValue) => setValue(newValue)}
+      onChange={(newValue) => updateData(newValue)}
     >
       <Editable onKeyDown={handleKeys(editor)} placeholder={"dump"} />
     </Slate>
--- src/main.tsx
+++ src/main.tsx
@@ -5,21 +5,11 @@ import App from "./App";
 import "firebase/firestore";
 import { FirebaseAppProvider } from "reactfire";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAtJ25nFF2VABsK5R27GJ3gcDbfODam67U",
-  authDomain: "qtags-6dade.firebaseapp.com",
-  projectId: "qtags-6dade",
-  storageBucket: "qtags-6dade.appspot.com",
-  messagingSenderId: "739622288531",
-  appId: "1:739622288531:web:9059f5cf128205a02c6166",
-  measurementId: "G-2W1F9SZHQJ",
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-      <App />
-    </FirebaseAppProvider>
+    {/* <FirebaseAppProvider firebaseConfig={firebaseConfig}> */}
+    <App />
+    {/* </FirebaseAppProvider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
