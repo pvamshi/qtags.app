5935385ccb632a2228a65a050ac454b5ada8b460
Vamshi Krishna Pothuka <pvamshi@gmail.com>
2021-06-13 01:04:26 +0200

    save login details

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "firebase/firestore";
-import { FirebaseAppProvider } from "reactfire";
 import firebase from "firebase";
 
 const firebaseConfig = {
@@ -18,44 +17,59 @@ const firebaseConfig = {
   measurementId: "G-TZYHCTL9GG",
 };
 firebase.initializeApp(firebaseConfig);
+const app = (user: firebase.User) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      {/* <FirebaseAppProvider firebaseConfig={firebaseConfig}> */}
+      <App uid={user.uid} />
+      {/* </FirebaseAppProvider> */}
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+// firebase
+//   .auth()
+//   .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+//   .then(() => {
 var provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    app(user);
+  } else {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential: firebase.auth.OAuthCredential | null =
+          result.credential;
 
-firebase
-  .auth()
-  .signInWithPopup(provider)
-  .then((result) => {
-    /** @type {firebase.auth.OAuthCredential} */
-    var credential: firebase.auth.OAuthCredential | null = result.credential;
-
-    if (credential !== null) {
-      var token = credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      if (user) {
-        ReactDOM.render(
-          <React.StrictMode>
-            {/* <FirebaseAppProvider firebaseConfig={firebaseConfig}> */}
-            <App uid={user.uid} />
-            {/* </FirebaseAppProvider> */}
-          </React.StrictMode>,
-          document.getElementById("root")
-        );
-      } else {
-        document.createElement("<p> Error while logging in</p>");
-      }
-    } else {
-      document.createElement("<p> Error while logging in</p>");
-    }
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    // ...
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+        if (credential !== null) {
+          var token = credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          if (user) {
+            app(user);
+          } else {
+            document.createElement("<p> Error while logging in</p>");
+          }
+        } else {
+          document.createElement("<p> Error while logging in</p>");
+        }
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    // });
+  }
+});
